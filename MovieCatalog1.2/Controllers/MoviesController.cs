59b86dea2e2b7df3d851b0e0809725bbbff99cf5
CozDoog101 <162032256+CozDoog101@users.noreply.github.com>
2025-03-31 21:13:09 +0300

    adding MoviesController

--- MovieCatalog1.2/Controllers/MoviesController.cs
+++ MovieCatalog1.2/Controllers/MoviesController.cs
@@ -2,9 +2,36 @@
 using Microsoft.AspNetCore.Mvc;
 using MovieCatalog1._2.Data.Models;
 using MovieCatalog1._2.Data;
+using MovieCatalog1._2.Models;
+using System.Diagnostics;
 
 namespace MovieCatalog1._2.Controllers
 {
-    
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 
 }
