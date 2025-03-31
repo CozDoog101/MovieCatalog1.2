11178c0bb4c47c613e3f855db4fbf3c2ef9179de
CozDoog101 <162032256+CozDoog101@users.noreply.github.com>
2025-03-31 21:28:01 +0300

    editing the MoviesController and adding cover prop in movies

--- MovieCatalog1.2/Controllers/MoviesController.cs
+++ MovieCatalog1.2/Controllers/MoviesController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MovieCatalog1._2.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using MovieCatalog1._2.Data;
-using MovieCatalog1._2.Models;
-using System.Diagnostics;
+using MovieCatalog1._2.Data.Models;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MovieCatalog1._2.Controllers
 {
@@ -16,22 +19,160 @@ namespace MovieCatalog1._2.Controllers
             _context = context;
         }
 
-        public MoviesController(ILogger<MoviesController> logger)
+        // GET: Movies
+        public async Task<IActionResult> Index()
         {
-            _logger = logger;
+            return View(await _context.Movies.ToListAsync());
         }
 
-        public IActionResult Index()
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
         {
             return View();
         }
 
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // POST: Movies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Genre,Year,Description,CreatedBy,Rating")] Movies movie, IFormFile CoverImageFile)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (ModelState.IsValid)
+            {
+                if (CoverImageFile != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(CoverImageFile.FileName);
+                    string filePath = Path.Combine("wwwroot/images/movies", fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await CoverImageFile.CopyToAsync(stream);
+                    }
+
+                    movie.CoverImage = "/images/movies/" + fileName;
+                }
+
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Genre,Year,Description,CreatedBy,Rating,CoverImage")] Movies movie, IFormFile CoverImageFile)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (CoverImageFile != null)
+                    {
+                        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(CoverImageFile.FileName);
+                        string filePath = Path.Combine("wwwroot/images/movies", fileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await CoverImageFile.CopyToAsync(stream);
+                        }
+
+                        movie.CoverImage = "/images/movies/" + fileName;
+                    }
+
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
         }
     }
-
 }
